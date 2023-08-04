aee6354c5b6f9a5db22e75777d3f41d7545d4b80
cgy <100090733+cgyinthehouse@users.noreply.github.com>
2023-08-04 19:18:55 +0800

    deploy express to vercel

--- api/index.js
+++ api/index.js
@@ -12,7 +12,7 @@ var cors = require("cors");
 app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static("public"));
+// app.use(express.static("public"));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function(_req, res) {
@@ -32,3 +32,4 @@ app.get("/api/whoami", function(req, res) {
 var listener = app.listen(process.env.PORT || 3000, function() {
   console.log("Your app is listening on port " + listener.address().port);
 });
+module.exports = app;

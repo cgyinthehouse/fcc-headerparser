9272051f1ae84f5b241196cfc367a8829f6ef065
cgy <100090733+cgyinthehouse@users.noreply.github.com>
2023-08-05 18:56:28 +0800

    make serving index.html works

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@
 require("dotenv").config();
 var express = require("express");
 var app = express();
+const path = require('path')
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC
@@ -12,11 +13,11 @@ var cors = require("cors");
 app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
-// app.use(express.static("public"));
+app.use(express.static("public"));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function(_req, res) {
-  res.sendFile(__dirname + "/views/index.html");
+  res.sendFile("index.html", { root: path.join(__dirname, "../public") });
 });
 
 // your first API endpoint...
